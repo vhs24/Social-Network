00daf4f95ffd123563ca651d3ba9fc4ea1f7cdd8
vhs24 <voson1024@gmail.com>
2024-03-08 16:56:03 +0700

    init

---
+++ Social-BE/Member-Service/src/test/java/com/se1/memberservice/MemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se1.memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Social-BE/Post-Service/src/test/java/com/se/postservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
